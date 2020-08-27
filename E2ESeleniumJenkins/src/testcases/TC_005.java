0216bc6ef5daabb3a2478c81f7431be6c75a15b3
SmritikonaSukul <SmritikonaSukul@206.253.242.96>
2020-08-27 20:28:17 +0530

    New Changes

---
+++ E2ESeleniumJenkins/src/testcases/TC_005.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_005 {
+
+	@Test
+	public void tc_1(){
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();                   // Maximizing browser window
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("Hello");
+		driver.findElement(By.id("pass")).sendKeys("Hello");
+		driver.quit();
+	}
+}
